04bda300dc264f87d59c38a7e4c05c1f7aaea292
Yuan-Sama <nbnguyen9557@gmail.com>
2024-11-05 13:59:46 +0700

    scaffold header

---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIcon, MatButton, MatBadge],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
