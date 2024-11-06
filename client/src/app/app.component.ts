0c04517a6b8afc78850604daa5befc7725a80ccc
Yuan-Sama <nbnguyen9557@gmail.com>
2024-11-06 15:40:21 +0700

    add type to product

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/product/model';
+import { Pagination } from './shared/model';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +14,13 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
   title = 'Skinet';
-  products: any[] = [];
+  products: Product[] = [];
 
   private http = inject(HttpClient);
 
   ngOnInit(): void {
-    this.http.get<any>(this.baseUrl + 'products').subscribe({
-      next: (response) => this.products = [...response.data],
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => (this.products = [...response.data]),
       error: (error) => console.error(error),
       complete: () => console.log('complete'),
     });
