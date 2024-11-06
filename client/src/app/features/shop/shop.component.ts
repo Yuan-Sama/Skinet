3adfe2fad0054cace6e986f3a0090cb8dfe883fd
Yuan-Sama <nbnguyen9557@gmail.com>
2024-11-07 00:41:24 +0700

    product component item

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Product } from '../../shared/product/model';
 import { ShopService } from '../../core/services/shop.service';
-import { MatCard } from '@angular/material/card';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [MatCard],
+  imports: [ProductItemComponent],
   templateUrl: './shop.component.html',
-})  
+})
 export class ShopComponent implements OnInit {
   products: Product[] = [];
 
