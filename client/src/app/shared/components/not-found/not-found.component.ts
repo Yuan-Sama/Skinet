6b598c3e2f572dbfdf2a092533593132962a5aa2
Yuan-Sama <nbnguyen9557@gmail.com>
2024-11-08 13:47:35 +0700

    style not found page

--- client/src/app/shared/components/not-found/not-found.component.ts
+++ client/src/app/shared/components/not-found/not-found.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html'
+  imports: [MatIcon, MatButton, RouterLink],
+  templateUrl: './not-found.component.html',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
