a509af08bc7395bbaa1e4146dc2462dfe1fb9c7c
Yuan-Sama <nbnguyen9557@gmail.com>
2024-11-08 11:17:56 +0700

    use snackbar

---
+++ client/src/app/shared/components/server-error/server-error.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-error.component.html'
+})
+export class ServerErrorComponent {
+
+}
